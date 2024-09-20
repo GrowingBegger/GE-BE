362d20d412191e71909bf4972dcd28e1b9ba54dc
HyunSu1768 <azxcv1768@gmail.com>
2024-09-20 11:15:03 +0900

    feat(Post.java): add userId and createdAt fields to Post entity for better data tracking and management chore(workspace.xml): clean up workspace.xml by removing unnecessary change entries and updating kind attributes for clarity

--- GE/src/main/java/org/example/ge/post/entity/Post.java
+++ GE/src/main/java/org/example/ge/post/entity/Post.java
@@ -1,6 +1,9 @@
 package org.example.ge.post.entity;
 
 import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class Post {
@@ -8,12 +11,19 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 45, nullable = false)
+    @Column(name = "user_id", nullable = false, updatable = false)
+    private Long userId;
+
+    @Column(name = "title", length = 45, nullable = false, updatable = false)
     private String title;
 
-    @Column(columnDefinition = "TEXT", nullable = false)
+    @Column(name = "content", columnDefinition = "TEXT", nullable = false, updatable = false)
     private String content;
 
-    @Column(length = 255, nullable = false)
+    @Column(name = "image_url", length = 255, nullable = false, updatable = false)
     private String imageUrl;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 }
