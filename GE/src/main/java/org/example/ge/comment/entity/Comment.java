cd76728942ab58ab9b607340b4c28f1e906049dd
HyunSu1768 <azxcv1768@gmail.com>
2024-09-20 11:12:01 +0900

    feat(comment): add Comment entity with JPA annotations for persistence chore(workspace): create workspace.xml for IDE configuration and settings

--- GE/src/main/java/org/example/ge/comment/entity/Comment.java
+++ GE/src/main/java/org/example/ge/comment/entity/Comment.java
@@ -1,2 +1,19 @@
-package org.example.ge.comment.entity;public class Comment {
+package org.example.ge.comment.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "post_id", nullable = false, updatable = false)
+    private Long postId;
+
+    @Column(name = "user_id", nullable = false, updatable = false)
+    private Long userId;
+
+    @Column(name = "content", nullable = false, length = 80, updatable = true)
+    private String content;
 }
