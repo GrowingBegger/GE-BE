34568a3563bc224c84de56673e6a9ec8e1122f4e
HyunSu1768 <azxcv1768@gmail.com>
2024-09-20 11:06:52 +0900

    feat(entity): add Post entity with JPA annotations for database mapping chore(.idea): initialize project configuration files for IDE support and Gradle integration

--- GE/src/main/java/org/example/ge/post/entity/Post.java
+++ GE/src/main/java/org/example/ge/post/entity/Post.java
@@ -1,2 +1,19 @@
-package org.example.ge.post.entity;public class Post {
+package org.example.ge.post.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 45, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(length = 255, nullable = false)
+    private String imageUrl;
 }
