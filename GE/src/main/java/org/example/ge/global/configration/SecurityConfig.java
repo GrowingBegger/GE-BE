c271aa19ee2b1c3970f22756c849e9ee4a86a98f
오송주 <dhthdwn7920@gmail.com>
2024-09-20 12:28:28 +0900

    setting: securityConfigration

---
+++ GE/src/main/java/org/example/ge/global/configration/SecurityConfig.java
@@ -0,0 +1,48 @@
+package org.example.ge.global.configration;
+
+import lombok.RequiredArgsConstructor;
+import org.example.ge.global.filter.JwtFilter;
+import org.example.ge.global.security.jwt.JwtParser;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtParser jwtParser;
+
+    @Bean
+    public static BCryptPasswordEncoder bCryptPasswordEncoder () { return new BCryptPasswordEncoder(); }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                    .httpBasic(AbstractHttpConfigurer::disable)
+                    .formLogin(AbstractHttpConfigurer::disable)
+
+                    .authorizeHttpRequests((auth) -> {
+                        auth.requestMatchers("/auth/login", "/user/signup", "/error").permitAll()
+                                .anyRequest().authenticated();
+                    })
+
+                    .sessionManagement(session
+                            -> { session.sessionCreationPolicy(SessionCreationPolicy.STATELESS); }
+                    )
+
+                    .headers(header
+                            -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+                    )
+                    .addFilterBefore(new JwtFilter(jwtParser), UsernamePasswordAuthenticationFilter.class)
+                    .build();
+    }
+}
