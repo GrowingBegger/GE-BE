fe7051fd8222adeb88328757506d3d26c5143fd1
HyunSu1768 <azxcv1768@gmail.com>
2024-10-11 11:13:37 +0900

    feat(CurrentUserProvider): add methods to retrieve current user details and roles for better user management functionality

--- GE/src/main/java/org/example/ge/instrastructure/common/user/CurrentUserProvider.java
+++ GE/src/main/java/org/example/ge/instrastructure/common/user/CurrentUserProvider.java
@@ -1,2 +1,13 @@
-package org.example.ge.instrastructure.common.user;public interface CurrentUserProvider {
+package org.example.ge.instrastructure.common.user;
+
+import java.util.List;
+
+public interface CurrentUserProvider {
+    Long getCurrentUserId();
+
+    String getCurrentUserName();
+
+    List<String> getCurrentUserRoles();
+
+    boolean isAuthenticated();
 }
