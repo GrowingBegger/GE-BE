a0f16188d193e5ee64044b5c14347ec70d0589b0
HyunSu1768 <azxcv1768@gmail.com>
2024-10-11 11:19:05 +0900

    feat(CreatePostService): add PostRepository to handle post persistence and prepare for image upload functionality

--- GE/src/main/java/org/example/ge/domain/post/sevice/CretePostService.java
+++ GE/src/main/java/org/example/ge/domain/post/sevice/CretePostService.java
@@ -2,13 +2,17 @@ package org.example.ge.domain.post.sevice;
 
 import lombok.RequiredArgsConstructor;
 import org.example.ge.domain.post.controller.dto.request.CreatePostRequest;
+import org.example.ge.domain.post.repository.PostRepository;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class CretePostService {
+    private final PostRepository postRepository;
+
     public void execute(CreatePostRequest request, Long userId) {
         // TODO :: 이미지 S3 업로드 로직 작성
-
+        String imageUrl = "";
+        postRepository.save(request.toEntity(userId, imageUrl));
     }
 }
