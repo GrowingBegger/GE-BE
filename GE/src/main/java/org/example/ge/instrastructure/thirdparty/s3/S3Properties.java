21d40c18ad6d61cea4cb4a0c006ded8486294c05
오송주 <dhthdwn7920@gmail.com>
2024-10-27 02:37:30 +0900

    fix: S3 관련 설정 오류 수정

--- GE/src/main/java/org/example/ge/instrastructure/thirdparty/s3/S3Properties.java
+++ GE/src/main/java/org/example/ge/instrastructure/thirdparty/s3/S3Properties.java
@@ -2,7 +2,7 @@ package org.example.ge.instrastructure.thirdparty.s3;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "cloud")
+@ConfigurationProperties(prefix = "cloud.aws")
 public record S3Properties(
         String bucket
 ) {
