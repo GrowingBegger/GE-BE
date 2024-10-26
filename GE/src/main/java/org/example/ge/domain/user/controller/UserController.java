682ffe352aa66179bea5a86e1d05a152591263d9
오송주 <dhthdwn7920@gmail.com>
2024-10-27 02:03:57 +0900

    feat: GetProfileResponse 기능개발

--- GE/src/main/java/org/example/ge/domain/user/controller/UserController.java
+++ GE/src/main/java/org/example/ge/domain/user/controller/UserController.java
@@ -3,7 +3,9 @@ package org.example.ge.domain.user.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.ge.domain.user.dto.request.LoginRequest;
 import org.example.ge.domain.user.dto.request.SignupRequest;
+import org.example.ge.domain.user.dto.response.GetProfileResponse;
 import org.example.ge.domain.user.dto.response.LoginResponse;
+import org.example.ge.domain.user.service.GetUserProfileService;
 import org.example.ge.domain.user.service.LoginService;
 import org.example.ge.domain.user.service.SignupService;
 import org.springframework.http.HttpStatus;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final LoginService loginService;
     private final SignupService signupService;
+    private final GetUserProfileService getProfileService;
 
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
@@ -26,5 +29,11 @@ public class UserController {
     @ResponseStatus(HttpStatus.CREATED)
     public LoginResponse signup(@RequestBody SignupRequest signupRequest) {
         return signupService.execute(signupRequest);
+    }
+
+    @GetMapping("/profile")
+    @ResponseStatus(HttpStatus.OK)
+    public GetProfileResponse getProfile() {
+        return getProfileService.execute();
     }
 }
