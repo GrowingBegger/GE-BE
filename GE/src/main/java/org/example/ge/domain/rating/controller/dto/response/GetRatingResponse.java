c84042d10749a423297a2663d8983452779f9c37
HyunSu1768 <azxcv1768@gmail.com>
2024-11-08 11:10:22 +0900

    feat(rating): 평점 조회 기능 추가 및 관련 클래스 수정 평점 조회를 위한 RatingController, RatingService 및 GetRatingResponse 클래스를 추가하여 사용자 ID에 따른 평점을 반환하는 기능을 구현.

--- GE/src/main/java/org/example/ge/domain/rating/controller/dto/response/GetRatingResponse.java
+++ GE/src/main/java/org/example/ge/domain/rating/controller/dto/response/GetRatingResponse.java
@@ -1,2 +1,6 @@
-package org.example.ge.domain.rating.controller.dto.response;public record GetRatingResponse() {
+package org.example.ge.domain.rating.controller.dto.response;
+
+public record GetRatingResponse(
+        Integer rating
+) {
 }
--- GE/src/main/java/org/example/ge/domain/rating/service/RatingService.java
+++ GE/src/main/java/org/example/ge/domain/rating/service/RatingService.java
@@ -1,2 +1,20 @@
-package org.example.ge.domain.rating.service;public class RatingService {
+package org.example.ge.domain.rating.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.ge.domain.rating.controller.dto.response.GetRatingResponse;
+import org.example.ge.domain.rating.entity.Rating;
+import org.example.ge.domain.rating.repository.RatingRepository;
+import org.example.ge.domain.user.exception.UserNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RatingService {
+    private final RatingRepository ratingRepository;
+    public GetRatingResponse getRating(Long userId) {
+        Rating rating = ratingRepository.findByUserId(userId).orElseThrow(() ->
+                UserNotFoundException.EXCEPTION);
+
+        return new GetRatingResponse(rating.getRating());
+    }
 }
